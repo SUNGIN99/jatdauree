c4ec99c09f53a571a3650d6d4b0a8a2f00f561f5
SUNGIN99 <99_insung@naver.com>
2023-07-05 23:54:44 +0900

    CORS 설정 테스트4 Webconfig 트러븘슈팅

--- src/main/java/com/example/jatdauree/src/WebConfig.java
+++ src/main/java/com/example/jatdauree/src/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
@@ -18,5 +18,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
-    }
+    }*/
 }
