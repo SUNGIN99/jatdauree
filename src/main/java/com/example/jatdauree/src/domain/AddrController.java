7b903a4e943a15c649a26cc417e33c47dfadbdc4
SUNGIN99 <99_insung@naver.com>
2023-07-17 03:22:20 +0900

    Proxy Header Check : 3 모든 요청에 대한 컨트롤러 생성 2

--- src/main/java/com/example/jatdauree/src/domain/AddrController.java
+++ src/main/java/com/example/jatdauree/src/domain/AddrController.java
@@ -3,6 +3,9 @@ package com.example.jatdauree.src.domain;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import static com.example.jatdauree.utils.comein.HttpReqRespUtils.allHeaderOutput;
+import static com.example.jatdauree.utils.comein.HttpReqRespUtils.getClientIpAddressIfServletRequestExist;
+
 @Slf4j
 @RestController
 @RequestMapping("/")
@@ -10,17 +13,26 @@ public class AddrController {
 
     @GetMapping("**") // 모든 요청에 대해 GET 매핑
     public String handleAllGetRequests() {
-        // GET 요청 처리 로직
+        String remoteAddr = getClientIpAddressIfServletRequestExist();
+        log.info("getIpAddIfserveletReqExist: " + remoteAddr);
+
+        allHeaderOutput();
         return "This is a GET request";
     }
     @PostMapping("**") // 모든 요청에 대해 GET 매핑
     public String handleAllPostRequests() {
-        // GET 요청 처리 로직
+        String remoteAddr = getClientIpAddressIfServletRequestExist();
+        log.info("getIpAddIfserveletReqExist: " + remoteAddr);
+
+        allHeaderOutput();
         return "This is a Post request";
     }
     @PatchMapping("**") // 모든 요청에 대해 GET 매핑
     public String handleAllPatchRequests() {
-        // GET 요청 처리 로직
+        String remoteAddr = getClientIpAddressIfServletRequestExist();
+        log.info("getIpAddIfserveletReqExist: " + remoteAddr);
+
+        allHeaderOutput();
         return "This is a Patch request";
     }
 
