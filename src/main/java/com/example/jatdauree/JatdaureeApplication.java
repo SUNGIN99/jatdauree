8a9edcf862e2d3b5548789e4fc71f6515b82847a
SUNGIN99 <99_insung@naver.com>
2023-07-24 01:10:14 +0900

    타임존설정

--- src/main/java/com/example/jatdauree/JatdaureeApplication.java
+++ src/main/java/com/example/jatdauree/JatdaureeApplication.java
@@ -7,10 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class JatdaureeApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 
 		SpringApplication.run(JatdaureeApplication.class, args);
