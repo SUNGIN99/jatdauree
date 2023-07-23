f9b3a41f0e787e70ed231f3020c446efa42172b8
SUNGIN99 <99_insung@naver.com>
2023-07-24 03:35:36 +0900

    세큐리티 권한별 설정

--- src/main/java/com/example/jatdauree/src/WebSecurityConfig.java
+++ src/main/java/com/example/jatdauree/src/WebSecurityConfig.java
@@ -23,10 +23,20 @@ WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                // 권한 테스트 URI ( 나중에삭제)
                 .antMatchers("/jat/sellers/test").hasRole("SELLER")
+                // app API (전부 요청 허락 23.07.24)
                 .antMatchers("/jat/app/**").permitAll()
-                .antMatchers("/jat/**").permitAll()
-                .anyRequest().authenticated()
+                // 관리자 요청 필터
+                .antMatchers("/jat/stores/admin/**").hasRole("ADMIN")
+                .antMatchers("/jat/reviews/admin").hasRole("ADMIN")
+                // 판매자 회원관련 API는 비밀번호 재설정빼고 토큰 필요 X
+                .antMatchers("/jat/sellers/pw-restore").hasRole("SELLER")
+                .antMatchers("/jat/sellers/**").permitAll()
+                // 그외 웹으로 들어오는 요청모두 권한 확인
+                .antMatchers("/jat/**").hasRole("SELLER")
+                // 말고 그냥 들어오는 모든 요청 거절절
+               .anyRequest().denyAll()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
